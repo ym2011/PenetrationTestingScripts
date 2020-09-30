0668c8c4a5781f79c12042f146fb1b608c11bde6
InfoSec <1241112575@qq.com>
2020-09-30 09:17:45 +0800

    Add files via upload

---
+++ Burpsuite/Run Burp Suite_v1.7.37.bat
@@ -0,0 +1,9 @@
+:: authour: ym2011
+:: time: 2020-8-19
+:: verison: 1.0
+@echo off
+title burpsuite pro is runnig...
+echo ****************************************
+echo ********burpsuite is starting***********
+echo ****************************************
+java -Xbootclasspath/p:burp-loader-keygen.jar -Xms4096M -jar burpsuite_pro_v1.7.37.jar
